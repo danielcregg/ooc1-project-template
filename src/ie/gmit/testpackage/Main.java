aae8ffe2e021f2edad99c2009cdd6fee42ecda0d
Daniel Cregg <daniel.cregg@gmit.ie>
2021-02-28 23:35:22 +0000

    Cleaning up for template

--- src/ie/gmit/testpackage/Main.java
+++ src/ie/gmit/testpackage/Main.java
@@ -1,46 +1,15 @@
 package ie.gmit.testpackage;
 
 /**
- * The Main class holds the main method.
+ * This is the Main class. It contains the main method.
  */
 public class Main {
-	
-	// Main method containing print statement.
+
+	// This is the main method. It contains a print statement.
 	public static void main(String[] args) {
-		
-		Student[] arrayOfStudents = new Student[10];
-		int randomAge = 0;
-		int maxAge = 23;
-		int minAge = 17;
-		float randomWeight = 0.0f;
-		float minWeight = 50.0f;
-		float maxWeight = 120.0f;
-		
-		for (int i = 0; i < arrayOfStudents.length; i++) {
-			arrayOfStudents[i] = new ScienceStudent();
-			randomAge = (int)(Math.random() * (maxAge - minAge + 1) + minAge);
-			arrayOfStudents[i].setAge(randomAge);
-			randomWeight = (float)(minWeight + Math.random() * (maxWeight - minWeight));
-			arrayOfStudents[i].setWeight(randomWeight);
-			System.out.print("Student " + i + " - ");
-			System.out.print("Age: " + arrayOfStudents[i].getAge() + ", ");
-			System.out.println("Weight: " + arrayOfStudents[i].getWeight());
-		}
-		
-		System.out.println("This is the main method, inside the Main calss.");
-		
-		// Create an object of type Student
-		Student scienceStudent1 = new ScienceStudent();
-		
-		// Execute the run method for Student object
-		scienceStudent1.iSpeak();
-		scienceStudent1.myCountry();
-		
-		LangStudent langStudent1 = new LangStudent();
-		langStudent1.setAge(20);
-		langStudent1.setLangOfStudy("French");
-		langStudent1.iSpeak();
-		
+
+		// This is a print statement.
+		System.out.println("This is the main method. I am inside the Main calss.");
+
 	} // End main method
-	
 } // End Main Class
