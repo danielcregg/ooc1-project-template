97ad39303bd79f61502122aa79cde5b492e3b8bb
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-11-20 11:50:03 +0000

    Simplify comments in Main.java and remove unnecessary documentation

--- src/ie/atu/testpackage/Main.java
+++ src/ie/atu/testpackage/Main.java
@@ -1,15 +1,9 @@
 package ie.atu.testpackage;
 
-/**
- * This is the Main class. It contains the main method.
- */
+ // This is the Main class. It contains the main method.
 public class Main {
-
-	// This is the main method. It contains a print statement.
+	// This is the main method.
 	public static void main(String[] args) {
-
-		// This is a print statement.
 		System.out.println("This is the main method. I am inside the Main calss.");
-
 	} // End main method
 } // End Main Class
